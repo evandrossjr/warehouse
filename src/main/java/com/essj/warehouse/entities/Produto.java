f3a936e76f66010624480608fb8eebc242ec7989
Evandro Sacramento <evandro-dev@outlook.com>
2025-06-12 13:31:36 -0300

    "adição da SerialVersion na classe Produto"

--- src/main/java/com/essj/warehouse/entities/Produto.java
+++ src/main/java/com/essj/warehouse/entities/Produto.java
@@ -13,6 +13,9 @@ import java.util.Objects;
 @Entity
 public class Produto implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
