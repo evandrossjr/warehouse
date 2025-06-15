252830e19d183b32e229ceb79ea0e152ea0d01e4
Evandro Sacramento II <evandro-dev@outlook.com>
2025-06-15 10:59:02 -0300

    "ProdutoRepository"

---
+++ src/main/java/com/essj/warehouse/repositories/ProdutoRepository.java
@@ -0,0 +1,7 @@
+package com.essj.warehouse.repositories;
+
+import com.essj.warehouse.entities.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
