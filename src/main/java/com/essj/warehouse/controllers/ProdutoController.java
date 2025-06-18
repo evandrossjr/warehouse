e7a628090934c00f2bedf22e957efc21d456a447
Evandro Sacramento II <evandro-dev@outlook.com>
2025-06-17 23:11:26 -0300

    ProdutoController findAll

--- src/main/java/com/essj/warehouse/controllers/ProdutoController.java
+++ src/main/java/com/essj/warehouse/controllers/ProdutoController.java
@@ -1,4 +1,25 @@
 package com.essj.warehouse.controllers;
 
+
+import com.essj.warehouse.dto.ProdutoMinDTO;
+import com.essj.warehouse.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/produtos")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    private List<ProdutoMinDTO> findAll(){
+        List<ProdutoMinDTO> result = produtoService.findAll();
+        return result;
+    }
 }
