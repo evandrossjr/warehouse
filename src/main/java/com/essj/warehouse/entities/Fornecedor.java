0cd7a0d1e23d1740e9698d676890de52927b5741
Evandro Sacramento II <evandro-dev@outlook.com>
2025-06-22 22:38:00 -0300

    Classe Fornecedor.java

---
+++ src/main/java/com/essj/warehouse/entities/Fornecedor.java
@@ -0,0 +1,60 @@
+package com.essj.warehouse.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Fornecedor {
+
+    @Id
+    private Long id;
+
+    private String razaoSocial;
+    private String nomeFantasia;
+    private String cnpj;
+
+
+
+    public Fornecedor(Long id, String razaoSocial, String nomeFantasia, String cnpj) {
+        this.id = id;
+        this.razaoSocial = razaoSocial;
+        this.nomeFantasia = nomeFantasia;
+        this.cnpj = cnpj;
+
+
+}
+
+    public Fornecedor() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
