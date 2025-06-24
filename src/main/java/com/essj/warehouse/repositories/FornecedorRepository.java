977b43e2599938a02400e8e00b07881f720f9c99
Evandro Sacramento II <evandro-dev@outlook.com>
2025-06-23 23:39:20 -0300

    FornecedorRepository

---
+++ src/main/java/com/essj/warehouse/repositories/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.essj.warehouse.repositories;
+
+import com.essj.warehouse.entities.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
+}
