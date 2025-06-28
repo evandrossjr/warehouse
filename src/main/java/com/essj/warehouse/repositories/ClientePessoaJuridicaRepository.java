585dc9f8aeee8a9bedd21eb4777b335fb57729d2
Evandro Sacramento II <evandro-dev@outlook.com>
2025-06-27 22:01:46 -0300

    ClientePessoaJuridicaRepository

---
+++ src/main/java/com/essj/warehouse/repositories/ClientePessoaJuridicaRepository.java
@@ -0,0 +1,7 @@
+package com.essj.warehouse.repositories;
+
+import com.essj.warehouse.entities.ClientePessoaJuridica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientePessoaJuridicaRepository extends JpaRepository<ClientePessoaJuridica, Long> {
+}
